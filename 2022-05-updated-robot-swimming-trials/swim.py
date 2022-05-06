344e3f9ba3890e92bd54c2bcebcdb80fb17fa660
Miguel <mbpereira94@gmail.com>
2022-05-06 19:01:26 +0100

    Fixes.

--- 2022-05-updated-robot-swimming-trials/swim.py
+++ 2022-05-updated-robot-swimming-trials/swim.py
@@ -1,6 +1,15 @@
 from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+import numpy as np
 
+# find lower bound for p
 e = lambda p: 1 - p - (1 - p/8)**24
 p = fsolve(e, 1)
-
 print(p)
+
+# plot function of p
+# x = np.linspace(0.95223967,1,100)
+# y = (1 - (1 - x/8)**24)/x
+# fig = plt.figure()
+# plt.plot(x, y)
+# plt.show()
