2b3fe2ef09e7fe4ce0936a7e5884489782584480
Miguel <mbpereira94@gmail.com>
2022-05-10 16:44:30 +0100

    Added solution for May 2022.

--- 2022-05-updated-robot-swimming-trials/swim.py
+++ 2022-05-updated-robot-swimming-trials/swim.py
@@ -2,18 +2,24 @@ from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import numpy as np
 
-# find lower bound for p
-e = lambda p: 1 - p - (1 - p/8)**24
-p = fsolve(e, 1)[0]
-print(p)
+# compute p
+e = lambda p: 1 - p - (1 - p/8)**24 + (7*p/8)**24
+p = fsolve(e, 0.98)[0]
+print('{:6f}'.format(p))
 
-# find upper bound for p
-u = (1/(3*0.334578))**(1/23)
-print(u)
-
-# plot function of p
-# x = np.linspace(0.95223967,1,100)
-# y = (1 - (1 - x/8)**24)/x
-# fig = plt.figure()
-# plt.plot(x, y)
-# plt.show()
+# plot probability that R1 wins as a function of p
+x_min = 0.98
+x_max = 1
+x = np.linspace(x_min, x_max, 100)
+y = (1 - (1 - x/8)**24 + (7*x/8)**24)/(3*x)
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.axhline(y=1/3, color='gray', linewidth=0.2)
+ax.axvline(x=p, color='gray', linewidth=0.2)
+ax.set_xticks([x_min, p, x_max])
+ax.set_xticklabels(['0.98', '{:6f}'.format(p), '1'])
+ax.set_yticks([1/3])
+ax.set_yticklabels(['1/3'])
+ax.set_xlabel('p')
+ax.set_ylabel('probability')
+plt.show()
