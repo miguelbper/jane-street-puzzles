41f8b705d1c1d2a6d0d5abb9b0e32ded3377b8f7
miguelbpgh <mbpereira94@gmail.com>
2022-05-31 12:49:53 +0200

    Used stirling from sympy.

--- 2021-10-robot-swimming-trials.py
+++ 2021-10-robot-swimming-trials.py
@@ -1,16 +1,14 @@
 from math import factorial
-from scipy.special import comb
+from sympy.functions.combinatorial.numbers import stirling
+from sympy import Float
 
 # stirling(n, k) = number of partitions of {1,...,n} into k sets
-def stirling(n, k):
-    return sum((-1)**i * comb(k, i) * (k - i)**n  for i in range(k+1)) / factorial(k)
-
 # probability that a robot playing uniform wins, if everyone else is playing discrete
 # = 1 - probability that it loses
 # = 1 - probability that every race is filled
 # = 1 - (number of ways of placing 3n-1 robots into n races, s.t. every race is filled) / (number of ways of placing 3n-1 robots into n races)
 def p(n):
-    return 1 - factorial(n) * stirling(3*n-1, n) / n**(3*n-1)
+    return Float(1 - factorial(n) * stirling(3*n-1, n) / n**(3*n-1))
 
 # print values
 for n in range(1,100):
