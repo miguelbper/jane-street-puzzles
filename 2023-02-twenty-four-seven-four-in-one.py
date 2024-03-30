cfa78b92982239be17b34faa82c4e8710d3716a4
Miguel Pereira <miguel.b.per@gmail.com>
2024-03-30 15:58:03 +0000

    Update 2023-02 to z3 with numpy

--- 2023-02-twenty-four-seven-four-in-one.py
+++ 2023-02-twenty-four-seven-four-in-one.py
@@ -2,10 +2,11 @@ from itertools import product
 from functools import reduce
 from operator import mul
 from typing import Optional
-from z3 import Solver, Int, And, Or, Not, Sum, PbEq, sat, BoolRef
+from z3 import Solver, And, Or, Not, PbEq, sat, BoolRef, IntVector, ModelRef
 from scipy.ndimage import label, sum_labels
 from pprint import pprint
 from codetiming import Timer
+import numpy as np
 
 
 # Parameters
@@ -14,7 +15,7 @@ Board = list[list[int]]
 Coord = tuple[int, int]
 
 z = -1
-matrix = [
+matrix = np.array([
     [z, z, z, z, z, z, z, z, z, z, z, z],
     [z, z, z, 1, z, z, z, z, z, 6, 5, z],
     [z, z, z, z, z, 3, z, z, z, z, 6, z],
@@ -27,14 +28,14 @@ matrix = [
     [z, 5, z, z, z, 7, z, z, z, z, z, z],
     [z, 6, 7, z, z, z, z, z, z, z, z, z],
     [z, z, z, z, 6, z, z, z, z, z, z, z],
-]
+])
 
-blue = [
+blue = np.array([
     [5,  7,  7, 33, 29, 2, 40, 28, z, z, 36, z],  # row
     [6,  z,  z,  4,  z, z,  z,  z, z, z,  z, 5],  # col rev
     [4,  z,  z,  1,  z, z,  z,  z, z, z,  z, 7],  # row rev
     [6, 36, 30, 34, 27, 3, 40, 27, z, z,  7, z],  # col
-]
+])
 
 R = list(range(12))
 IJ = list(product(R, repeat=2))
@@ -62,6 +63,11 @@ def areas(xm: Board) -> int:
     return int(reduce(mul, area))
 
 
+def evaluate_vars(m: ModelRef, vars: np.ndarray) -> np.ndarray:
+    ''' Evaluate variables in a z3 model. '''
+    return np.vectorize(lambda x: m.evaluate(x).as_long())(vars)
+
+
 def grid_ranges(grid_num: int) -> tuple[int, int, int, int]:
     ''' Return index bounds for a given 7x7 grid index. '''
     i0 =  5 if ((grid_num + 1) % 4 > 1) else 0
@@ -83,70 +89,64 @@ def rowcol_coords(side_idx: int, rowcol_idx: int) -> list[Coord]:
 
 # Solver & constraints
 # ----------------------------------------------------------------------
-X = [[Int(f'x_[{i}, {j}]') for j in R] for i in R]
+X = np.array(IntVector('x', 12**2)).reshape((12, 12))
 s = Solver()
 
 # Ranges for each variable
-s.add([And(0 <= X[i][j], X[i][j] <= 7) for i, j in IJ])
+s += [And(0 <= x, x <= 7) for x in X.flat]
 
 # Given numbers
-for i, j in IJ:
-    if matrix[i][j] != z:
-        s.add(X[i][j] == matrix[i][j])
+s += [x == v for x, v in zip(X.flat, matrix.flat) if v != z]
 
 # Each 7-by-7 grid should contain one 1, two 2’s, etc., up to seven 7’s
 for grid_num in range(4):
     i0, i1, j0, j1 = grid_ranges(grid_num)
     grid = list(product(range(i0, i1), range(j0, j1)))
-    for x in range(1, 8):
-        s.add(PbEq([(X[i][j] == x, 1) for i, j in grid], x))
+    s += [PbEq([(X[i][j] == x, 1) for i, j in grid], x) for x in range(1, 8)]
 
 # Each row/col within the 7-by-7’s must contain exactly 4 nums which sum to 20
 for grid_num in range(4):
     i0, i1, j0, j1 = grid_ranges(grid_num)
-    # rows
-    for i in range(i0, i1):
-        s.add(PbEq([(X[i][j] != 0, 1) for j in range(j0, j1)], 4))
-        s.add(Sum([X[i][j] for j in range(j0, j1)]) == 20)
-    # cols
-    for j in range(j0, j1):
-        s.add(PbEq([(X[i][j] != 0, 1) for i in range(i0, i1)], 4))
-        s.add(Sum([X[i][j] for i in range(i0, i1)]) == 20)
+    X_subgrid = X[i0:i1, j0:j1]
+    for i in range(7):
+        s += sum(X_subgrid[i, :]) == 20
+        s += sum(X_subgrid[:, i]) == 20
+        s += PbEq([(x != 0, 1) for x in X_subgrid[i, :]], 4)
+        s += PbEq([(x != 0, 1) for x in X_subgrid[:, i]], 4)
 
 # Every 2-by-2 subsquare in the grid must contain at least one empty cell
 for i, j in product(range(11), repeat=2):
     dirs = [(0, 0), (0, 1), (1, 0), (1, 1)]
-    s.add(Or([X[i + di][j + dj] == 0 for di, dj in dirs]))
+    s += Or([X[i + di][j + dj] == 0 for di, dj in dirs])
 
 # A number <= 7 represents the value of the first number it sees
 def first_num_seen(coords: list[Coord], blue_num: int, k: int) -> BoolRef:
-    zeros_before_k = And([X[i][j] == 0 for i, j in coords[:k]])
-    first_num = X[coords[k][0]][coords[k][1]] == blue_num
+    zeros_before_k = And([X[i, j] == 0 for i, j in coords[:k]])
+    first_num = X[coords[k][0], coords[k][1]] == blue_num
     return And(zeros_before_k, first_num)
 
 for side_idx, side_nums in enumerate(blue):
     for rowcol_idx, blue_num in enumerate(side_nums):
         if z < blue_num <= 7:
             coords = rowcol_coords(side_idx, rowcol_idx)
-            s.add(Or([first_num_seen(coords, blue_num, k) for k in range(4)]))
+            s += Or([first_num_seen(coords, blue_num, k) for k in range(4)])
 
 # A number > 7 represents the sum of the row or column it is facing
 for side_idx, side_nums in enumerate(blue):
     for rowcol_idx, blue_num in enumerate(side_nums):
         if blue_num > 7:
             coords = rowcol_coords(side_idx, rowcol_idx)
-            s.add(Sum([X[i][j] for i, j in coords]) == blue_num)
+            s += sum(X[i][j] for i, j in coords) == blue_num
 
-# The numbered cells must form a connected region
 # Wrap solver in a function which also checks connectivity
 def solve(s: Solver) -> Optional[Board]:
     while s.check() == sat:
         m = s.model()
-        xm = [[m.evaluate(X[i][j]).as_long() for j in R] for i in R]
+        xm = evaluate_vars(m, X)
         if connected(xm):
-            return [[xm[i][j] for j in R] for i in R]
+            return xm
         else:
-            s.add(Not(And([X[i][j] == xm[i][j] for i, j in IJ])))
+            s += Not(And([x == y for x, y in zip(X.flat, xm.flat)]))
     return None
 
 
@@ -155,7 +155,7 @@ def solve(s: Solver) -> Optional[Board]:
 with Timer():
     xm = solve(s)
 
-if xm:
+if xm is not None:
     ans = areas(xm)
     print(f'ans = {areas(xm)}\nboard = ')
     pprint(xm)
