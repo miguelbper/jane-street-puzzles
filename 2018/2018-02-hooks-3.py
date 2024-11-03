522ac3c9cf95639f73808aaf454aaed81ff94a9b
Miguel Pereira <miguel.b.per@gmail.com>
2024-11-03 16:29:38 +0000

    Format 2018

--- 2018/2018-02-hooks-3.py
+++ 2018/2018-02-hooks-3.py
@@ -1,24 +1,24 @@
-from itertools import product
 from functools import reduce
+from itertools import product
 from operator import mul
-from typing import Optional
-from z3 import (Solver, And, Or, Not, Implies, If, PbEq, sat, BoolRef,
-                IntVector, ModelRef)
-from scipy.ndimage import label, sum_labels
 from pprint import pprint
-from codetiming import Timer
-import numpy as np
 
+import numpy as np
+from codetiming import Timer
+from scipy.ndimage import label, sum_labels
+from z3 import And, BoolRef, If, Implies, IntVector, ModelRef, Not, Or, PbEq, Solver, sat
 
 # Parameters
 # ----------------------------------------------------------------------
 
+# fmt: off
 clues = np.array([
     [0, 49, 63,  0, 18, 42, 63, 54, 0],
     [0, 35, 42, 18, 18,  0, 36, 63, 0],
     [0, 56,  0, 32, 40, 15, 16, 25, 0],
     [0, 40, 32, 40, 10, 12,  0, 56, 0],
 ])
+# fmt: on
 n = clues.shape[1]
 Board = np.ndarray[(n, n), int]
 Coord = tuple[int, int]
@@ -27,19 +27,19 @@ Coord = tuple[int, int]
 # Utility functions
 # ----------------------------------------------------------------------
 def neighbours(i: int, j: int) -> list[Coord]:
-    ''' Return the coordinates of the neighbours of (i, j). '''
+    """Return the coordinates of the neighbours of (i, j)."""
     direc = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     neigh = [(i + di, j + dj) for di, dj in direc]
     return [(x, y) for x, y in neigh if 0 <= x < n and 0 <= y < n]
 
 
 def connected(xm: Board) -> bool:
-    ''' True iff the board is connected. '''
+    """True iff the board is connected."""
     return label(xm)[1] <= 1
 
 
 def areas(xm: Board) -> int:
-    ''' Product of the areas of the unfilled regions. '''
+    """Product of the areas of the unfilled regions."""
     mat = np.where(xm == 0, 1, 0)
     labels, k = label(mat)
     area = sum_labels(mat, labels, index=range(1, k + 1))
@@ -47,19 +47,19 @@ def areas(xm: Board) -> int:
 
 
 def evaluate_vars(m: ModelRef, vars: np.ndarray) -> np.ndarray:
-    ''' Evaluate variables in a z3 model. '''
+    """Evaluate variables in a z3 model."""
     return np.vectorize(lambda x: m.evaluate(x).as_long())(vars)
 
 
 # Variables, solver & constraints
 # ----------------------------------------------------------------------
 s = Solver()
-X = np.array(IntVector('x', n**2)).reshape((n, n))  # nums in grid
-O = IntVector('o', n)                               # hook orientation
+X = np.array(IntVector("x", n**2)).reshape((n, n))  # nums in grid
+O = IntVector("o", n)  # hook orientation
 
 # Ranges for each variable
-s += [And(0 <= x, x <= n) for x in X.flat]
-s += [And(0 <= o, o <= 3) for o in O]
+s += [And(x >= 0, x <= n) for x in X.flat]
+s += [And(o >= 0, o <= 3) for o in O]
 
 # Every 2-by-2 region must contain at least one unfilled square
 for i, j in product(range(n - 1), repeat=2):
@@ -69,9 +69,10 @@ for i, j in product(range(n - 1), repeat=2):
 # There are d d's in the board
 s += [PbEq([(x == d, 1) for x in X.flat], d) for d in range(1, n + 1)]
 
+
 # The number in the kth hook is n - k
 def in_hook(i: int, j: int, k: int) -> BoolRef:
-    '''z3 bool expression: True <=> (i, j) is in the k-th hook.'''
+    """z3 bool expression: True <=> (i, j) is in the k-th hook."""
     i0 = sum(If(Or(o == 0, o == 3), 1, 0) for o in O[:k])
     j0 = sum(If(Or(o == 0, o == 1), 1, 0) for o in O[:k])
     i1 = i0 + n - k
@@ -82,13 +83,15 @@ def in_hook(i: int, j: int, k: int) -> BoolRef:
     in_col = And(j == col, i0 <= i, i < i1)
     return Or(in_row, in_col)
 
+
 for i, j, k in product(range(n), repeat=3):
     s += Implies(in_hook(i, j, k), Or(X[i, j] == 0, X[i, j] == n - k))
 
+
 # Number outside the grid = product of the first 2 numbers visible
 def visible_product(side: int, clue_idx: int, clue: int) -> BoolRef:
-    '''z3 bool expression: True <=> the product of the first 2 numbers
-    visible from the side is 'clue'.'''
+    """z3 bool expression: True <=> the product of the first 2 numbers
+    visible from the side is 'clue'."""
     # Array visible from side at clue_dix
     dim = side % 2
     X_arr = np.take(X, indices=clue_idx, axis=dim)
@@ -96,9 +99,9 @@ def visible_product(side: int, clue_idx: int, clue: int) -> BoolRef:
         X_arr = X_arr[::-1]
 
     def case_first_visible(k: int, l: int) -> BoolRef:
-        '''Case where first 2 visible nums are at k and l.'''
+        """Case where first 2 visible nums are at k and l."""
         first_k_empty = And([x == 0 for x in X_arr[:k]])
-        k_to_l_empty = And([x == 0 for x in X_arr[k+1:l]])
+        k_to_l_empty = And([x == 0 for x in X_arr[k + 1 : l]])
 
         possibilities = []
         for x in range(1, 10):
@@ -115,6 +118,7 @@ def visible_product(side: int, clue_idx: int, clue: int) -> BoolRef:
 
     return Or([case_first_visible(k, l) for l in range(n) for k in range(l)])
 
+
 for side in range(4):
     for clue_idx, clue in enumerate(clues[side]):
         if clue:
@@ -125,8 +129,9 @@ for i, j in product(range(n), repeat=2):
     neigh = neighbours(i, j)
     s += Implies(X[i, j] != 0, Or([X[a, b] != 0 for a, b in neigh]))
 
+
 # Wrap solver in a function which also checks connectivity
-def solve(s: Solver) -> Optional[Board]:
+def solve(s: Solver) -> Board | None:
     while s.check() == sat:
         m = s.model()
         xm = evaluate_vars(m, X)
@@ -144,21 +149,19 @@ with Timer():
 
 if xm is not None:
     ans = areas(xm)
-    print(f'ans = {ans}\nboard = ')
+    print(f"ans = {ans}\nboard = ")
     pprint(xm)
 else:
-    print('No solution found.')
-'''
-Elapsed time: 1.2200 seconds
-ans = 20736
-board =
-array([[0, 0, 0, 0, 9, 9, 0, 9, 0],
-       [8, 7, 7, 0, 0, 7, 0, 7, 0],
-       [0, 0, 6, 6, 0, 6, 6, 7, 9],
-       [8, 0, 4, 0, 0, 1, 0, 0, 9],
-       [8, 5, 4, 3, 2, 2, 0, 0, 9],
-       [0, 5, 0, 3, 0, 3, 6, 7, 0],
-       [0, 0, 0, 4, 0, 4, 0, 7, 9],
-       [0, 5, 0, 5, 5, 0, 6, 0, 9],
-       [0, 8, 8, 8, 0, 0, 8, 8, 9]])
-'''
+    print("No solution found.")
+# Elapsed time: 1.2200 seconds
+# ans = 20736
+# board =
+# array([[0, 0, 0, 0, 9, 9, 0, 9, 0],
+#        [8, 7, 7, 0, 0, 7, 0, 7, 0],
+#        [0, 0, 6, 6, 0, 6, 6, 7, 9],
+#        [8, 0, 4, 0, 0, 1, 0, 0, 9],
+#        [8, 5, 4, 3, 2, 2, 0, 0, 9],
+#        [0, 5, 0, 3, 0, 3, 6, 7, 0],
+#        [0, 0, 0, 4, 0, 4, 0, 7, 9],
+#        [0, 5, 0, 5, 5, 0, 6, 0, 9],
+#        [0, 8, 8, 8, 0, 0, 8, 8, 9]])
--- 2018/2018-06-twenty-four-seven.py
+++ 2018/2018-06-twenty-four-seven.py
@@ -1,24 +1,25 @@
-from z3 import Solver, IntVector, PbEq, Or, sat, Not, And, ModelRef
-import numpy as np
-from itertools import product
-from typing import Optional
-from scipy.ndimage import label, sum_labels
-from codetiming import Timer
-from operator import mul
 from functools import reduce
+from itertools import product
+from operator import mul
 
+import numpy as np
+from codetiming import Timer
+from scipy.ndimage import label, sum_labels
+from z3 import And, IntVector, ModelRef, Not, Or, PbEq, Solver, sat
 
 # Parameters
 # ----------------------------------------------------------------------
-grid = np.array([
-    [0, 4, 0, 0, 0, 0, 0],
-    [0, 0, 6, 3, 0, 0, 6],
-    [0, 0, 0, 0, 0, 5, 5],
-    [0, 0, 0, 4, 0, 0, 0],
-    [4, 7, 0, 0, 0, 0, 0],
-    [2, 0, 0, 7, 4, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0],
-])
+grid = np.array(
+    [
+        [0, 4, 0, 0, 0, 0, 0],
+        [0, 0, 6, 3, 0, 0, 6],
+        [0, 0, 0, 0, 0, 5, 5],
+        [0, 0, 0, 4, 0, 0, 0],
+        [4, 7, 0, 0, 0, 0, 0],
+        [2, 0, 0, 7, 4, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0],
+    ]
+)
 n = grid.shape[0]
 Board = np.ndarray[(n, n), int]
 
@@ -26,12 +27,12 @@ Board = np.ndarray[(n, n), int]
 # Utility functions
 # ----------------------------------------------------------------------
 def connected(xm: Board) -> bool:
-    ''' True iff the board is connected. '''
+    """True iff the board is connected."""
     return label(xm)[1] <= 1
 
 
 def areas(xm: Board) -> int:
-    ''' Product of the areas of the unfilled regions. '''
+    """Product of the areas of the unfilled regions."""
     mat = np.where(xm == 0, 1, 0)
     labels, k = label(mat)
     area = sum_labels(mat, labels, index=range(1, k + 1))
@@ -39,17 +40,17 @@ def areas(xm: Board) -> int:
 
 
 def evaluate_vars(m: ModelRef, vars: np.ndarray) -> np.ndarray:
-    ''' Evaluate variables in a z3 model. '''
+    """Evaluate variables in a z3 model."""
     return np.vectorize(lambda x: m.evaluate(x).as_long())(vars)
 
 
 # Variables, solver, constraints
 # ----------------------------------------------------------------------
-X = np.array(IntVector('x', n**2)).reshape(n, n)
+X = np.array(IntVector("x", n**2)).reshape(n, n)
 s = Solver()
 
 # Given numbers
-s += [x == v for x, v in zip(X.flat, grid.flat) if v]
+s += [x == v for x, v in zip(X.flat, grid.flat, strict=False) if v]
 
 # Grid contains one 1, two 2’s, etc., up to seven 7’s
 s += [PbEq([(x == k, 1) for x in X.flat], k) for k in range(1, 8)]
@@ -66,15 +67,16 @@ for i, j in product(range(n - 1), repeat=2):
     dirs = [(0, 0), (0, 1), (1, 0), (1, 1)]
     s += Or([X[i + di, j + dj] == 0 for di, dj in dirs])
 
+
 # Wrap solver in a function which also checks connectivity
-def solve(s: Solver) -> Optional[Board]:
+def solve(s: Solver) -> Board | None:
     while s.check() == sat:
         m = s.model()
         xm = evaluate_vars(m, X)
         if connected(xm):
             return xm
         else:
-            s += Not(And([x == y for x, y in zip(X.flat, xm.flat)]))
+            s += Not(And([x == y for x, y in zip(X.flat, xm.flat, strict=False)]))
     return None
 
 
@@ -85,19 +87,17 @@ with Timer():
 
 if xm is not None:
     ans = areas(xm)
-    print(f'ans = {ans}\nboard = ')
+    print(f"ans = {ans}\nboard = ")
     print(xm)
 else:
-    print('No solution found.')
-'''
-Elapsed time: 0.5000 seconds
-ans = 240
-board =
-[[7 4 3 0 6 0 0]
- [0 0 6 3 5 0 6]
- [0 0 5 0 5 5 5]
- [0 3 6 4 0 0 7]
- [4 7 0 0 0 7 2]
- [2 0 0 7 4 7 0]
- [7 6 0 6 0 1 0]]
-'''
+    print("No solution found.")
+# Elapsed time: 0.5000 seconds
+# ans = 240
+# board =
+# [[7 4 3 0 6 0 0]
+#  [0 0 6 3 5 0 6]
+#  [0 0 5 0 5 5 5]
+#  [0 3 6 4 0 0 7]
+#  [4 7 0 0 0 7 2]
+#  [2 0 0 7 4 7 0]
+#  [7 6 0 6 0 1 0]]
