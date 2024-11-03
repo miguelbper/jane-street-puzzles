a9a5f3a73db1e10d3c4ccec1a2f02573ef6dabb9
Miguel Pereira <miguel.b.per@gmail.com>
2024-11-03 16:58:07 +0000

    Format 2020, add PyQt6 and allow random in bandit

--- 2020/2020-06-circle-time.py
+++ 2020/2020-06-circle-time.py
@@ -1,11 +1,6 @@
-from sympy import solve, pi, Rational
-from sympy.abc import x, y, z, a, p
-
-'''
-Assume that the large circle has radius 1. Each circle in the ring has
-radius r = 1/3. Let
-    a = maximum area we can cover
-    p = maximum proportion we can cover = a / pi
+"""Assume that the large circle has radius 1. Each circle in the ring has
+radius r = 1/3. Let a = maximum area we can cover p = maximum proportion we can
+cover = a / pi.
 
 Then,
     a = 6 * pi * r**2 + x**2 * a,
@@ -22,21 +17,23 @@ Then, using trigonometry, we have the following equations
     3*(y + z) = 1   (1)               [definition of y]
     (2 * x)**2 = x**2 + 9*z**2        [Pythagorean theorem]
     (1 + x)**2 = (1 + 3*y)**2 + x**2  [Pythagorean theorem]
-'''
+"""
 
+from sympy import Rational, pi, solve
+from sympy.abc import a, x, y, z
 
 # Find x = radius of large inner circle
 # ----------------------------------------------------------------------
 eqs_xyz = [
-    3*(y + z) - 1,
-    (2 * x)**2 - x**2 - 9*z**2,
-    (1 + x)**2 - (1 + 3*y)**2 - x**2,
+    3 * (y + z) - 1,
+    (2 * x) ** 2 - x**2 - 9 * z**2,
+    (1 + x) ** 2 - (1 + 3 * y) ** 2 - x**2,
 ]
 
 solutions = solve(eqs_xyz, [x, y, z])
 valid_solutions = [s for s in solutions if all(v.is_positive for v in s)]
 x, _, _ = valid_solutions[0]
-print(f'{x = }')
+print(f"{x = }")
 # x = -2*sqrt(1 + sqrt(3))/3 + 1/3 + 2*sqrt(3)/3
 
 
@@ -47,10 +44,10 @@ print(f'{x = }')
 r = Rational(1, 3)
 eq_a = a - (6 * pi * r**2 + x**2 * a)
 a = solve(eq_a, a)[0]
-print(f'{a = }')
+print(f"{a = }")
 # a = 6*pi/(9 - (-2*sqrt(1 + sqrt(3)) + 1 + 2*sqrt(3))**2)
 
 # Find proportion
 p = a / pi
-print(f'{p = }', f'\n  = {p.evalf():.6f}')
+print(f"{p = }", f"\n  = {p.evalf():.6f}")
 # p = 6/(9 - (-2*sqrt(1 + sqrt(3)) + 1 + 2*sqrt(3))**2) = 0.783464
--- 2020/2020-10-candy-collectors.py
+++ 2020/2020-10-candy-collectors.py
@@ -1,10 +1,4 @@
-from math import factorial, comb
-from fractions import Fraction
-from sympy.abc import a, b, c, d, e
-from sympy import Poly
-
-
-'''
+"""
 This solution is based on the math.stackexchange post
 https://math.stackexchange.com/questions/3934750/how-to-solve-this-candy-collectors-puzzle
 
@@ -47,15 +41,24 @@ I.e., f1 is given by the formula implemented in code below.
 Define polys for the other types of candy f2, f3, f4, f5 analogously.
 
 Then, count = coef of (abcde)^5 of f1*f2*f3*f4*f5
-'''
+"""
+
+from fractions import Fraction
+from math import comb, factorial
+
+from sympy import Poly
+from sympy.abc import a, b, c, d, e
 
 
 def f(a, b, c, d, e):
     T5 = comb(5, 5) * a**5
     T4 = comb(5, 4) * a**4 * (b + c + d + e)
-    T3 = comb(5, 3) * a**3 * ((b**2 + c**2 + d**2 + e**2)
-                              + 2 * (b*c + b*d + b*e + c*d + c*e + d*e))
-    T2 = comb(5, 2) * a**2 * factorial(3) * (c*d*e + b*d*e + b*c*e + b*c*d)
+    T3 = (
+        comb(5, 3)
+        * a**3
+        * ((b**2 + c**2 + d**2 + e**2) + 2 * (b * c + b * d + b * e + c * d + c * e + d * e))
+    )
+    T2 = comb(5, 2) * a**2 * factorial(3) * (c * d * e + b * d * e + b * c * e + b * c * d)
     return T5 + T4 + T3 + T2
 
 
@@ -64,11 +67,11 @@ f2 = Poly(f(b, c, d, e, a))
 f3 = Poly(f(c, d, e, a, b))
 f4 = Poly(f(d, e, a, b, c))
 f5 = Poly(f(e, a, b, c, d))
-gf = f1*f2*f3*f4*f5
+gf = f1 * f2 * f3 * f4 * f5
 
-count = gf.coeff_monomial((a*b*c*d*e)**5)
-den = Fraction(factorial(5**2), (factorial(5))**5)
+count = gf.coeff_monomial((a * b * c * d * e) ** 5)
+den = Fraction(factorial(5**2), (factorial(5)) ** 5)
 num = factorial(5) * count
 p = Fraction(num, den)
-print(f'p = {p}')
+print(f"p = {p}")
 # p = 318281087/8016470462
