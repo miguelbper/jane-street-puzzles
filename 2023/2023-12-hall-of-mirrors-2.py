e055a5c0717cb9f1c2cd100f4541a717a325a044
Miguel Pereira <miguel.pereira@signature.ai>
2025-01-25 22:17:20 +0000

    Add 2023-12

---
+++ 2023/2023-12-hall-of-mirrors-2.py
@@ -0,0 +1,18 @@
+"""
+No programming is required for this puzzle, so this is just a text file.
+
+Step 1: Light ray
+- The puzzle name suggests that the small dashes are mirrors
+- Notice that all mirror angles are such that light rays move at an
+  angle which is a multiple of 45 degrees.
+- There is a "special" light ray which starts and ends at the two mirrors
+  which are misaligned from the center of their cell.
+- Draw this light ray on the grid.
+
+Step 2: Message
+- The whole grid can be though of as a 4x4 grid of squares.
+- Inside each square, there are two lines drawn by the light ray.
+- The yellow/red coloring suggests the flag semaphore system.
+- Translating each square into a letter using this system, we get the message:
+SEASON'S GREETINGS
+"""
